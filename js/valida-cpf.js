dcb07da9068e4436faf07cdfa470d2fee494a283
swtrmdwyu <swtrmdwyu@gmail.com>
2023-09-21 15:38:55 -0300

    validação de cpf.

---
+++ js/valida-cpf.js
@@ -0,0 +1,62 @@
+export default function isCpf(campo) {
+    const cpf = campo.value.replace(/\.|-/g, "");
+
+    if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        campo.setCustomValidity('Esse cpf não é válido');
+    };
+};
+
+
+function validaNumerosRepetidos(cpf) {
+    
+    const numerosRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ];
+
+    return numerosRepetidos.includes(cpf);
+};
+
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for(let i = 0; i < 9; i++) {
+        soma += cpf[i] * multiplicador;
+        multiplicador--;
+    };
+
+    soma = (soma * 10) % 11;
+
+    if(soma == 10 || soma == 11) {
+        soma = 0;
+    };
+
+    return soma != cpf[9];
+};
+
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for(let i = 0; i < 10; i++) {
+        soma += cpf[i] * multiplicador;
+        multiplicador--;
+    };
+
+    soma = (soma * 10) % 11;
+
+    if(soma == 10 || soma == 11) {
+        soma = 0;
+    };
+
+    return soma != cpf[10];
+};
